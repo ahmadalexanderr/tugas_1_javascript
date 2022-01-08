391ad70373ca75fa988ed2ce71e0ef549bb31a7c
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-08 14:35:46 +0700

    Upload tugas 1 js

---
+++ tugas1.js
@@ -0,0 +1,14 @@
+var firstName = "Ahmad";
+let lastName = "Alexander";
+const age = "20";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+// if(true){
+//     lastName = "Alex";
+//     console.log(lastName);
+// }
+
+// console.log(lastName);
